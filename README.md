878a8958f754832f0e58c4c4214ee323fc61a098
Ori Garibi <garibi@chapman.edu>
2020-11-22 20:04:31 -0800

    Adds numbers.sh and README.md

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Identifying Information: 
-1. Name: 
-2. Student ID: 
-3. Email: 
-4. Class: 
+1. Name: Ori Garibi 
+2. Student ID: 2367830
+3. Email: garibi@chapman.edu
+4. Class: CENG 298-01
 5. Assignment: numbers 
 
 ## Application Purpose
--- numbers.sh
+++ numbers.sh
@@ -1,1 +1,17 @@
-hello
+#! /bin/bash
+#numbers.sh
+#Ori Garibi
+#November 22, 2020
+read -p "Enter a number: " N
+num=1
+start=1
+for (( num=$start;  num<=$N; c++ ))
+do
+	if [ $((num%2)) -eq 0 ]
+	then
+		echo "$num EVEN"
+	else
+		echo "$num ODD"
+	fi
+	num=$[$num+1]
+done
